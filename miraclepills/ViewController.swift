416234a732624d26db02ae8c49b74c88c6e623da
gagandeep <gagandeepkaur01@yahoo.com>
2021-05-06 18:41:46 +0530

    Initial Commit

---
+++ miraclepills/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  miraclepills
+//
+//  Created by user181181 on 5/6/21.
+//  Copyright © 2021 user181181. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
